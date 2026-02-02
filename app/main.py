e30e2e68777c36374144f6fae838781cee6869d5
Rachel Armstrong <rarmstrong573@gmail.com>
2026-02-02 13:30:09 +0000

    add echo if input not valid

--- app/main.py
+++ app/main.py
@@ -1,8 +1,14 @@
 import sys
 
-
 def main():
     sys.stdout.write("$ ")
+
+    raw_input = input()
+
+    input_not_valid = True
+
+    if input_not_valid:
+        sys.stdout.write(f"{raw_input}: command not found")
     pass
 
 
