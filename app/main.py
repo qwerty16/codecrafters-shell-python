6263970b9da6aafa4975500d556667891a05bebb
Rachel Armstrong <rarmstrong573@gmail.com>
2026-02-05 08:50:50 +0000

    restructure

--- app/main.py
+++ app/main.py
@@ -1,15 +1,36 @@
 import sys
 import os
 import subprocess
+from typing import TypeAlias, Union
 
-BUILTINS = ["exit", "echo", "type", "pwd"]
+Response: TypeAlias = Union[str, False]
+
+
+def exit(*args, **kwargs) -> Response:
+    return False
+
+
+def pwd(*args, **kwargs) -> Response:
+    return os.getcwd()
+
+
+def echo(*args, **kwargs) -> Response:
+    return " ".join(args[1:])
+
+
+def type(*args, **kwargs) -> Response:
+    second_command = args[1]
+    executable_found = find_executable_path(second_command)
+
+    if second_command in BUILTINS:
+        return f"{second_command} is a shell builtin"
+    elif executable_found:
+        return f"{second_command} is {executable_found}"
+    else:
+        return f"{second_command}: not found"
 
 
 def find_executable_path(command: str):
-    # Check for being in builtins
-    if command in BUILTINS:
-        return command
-
     search_paths = os.environ["PATH"].split(os.pathsep)
 
     for search_path in search_paths:
@@ -21,54 +42,43 @@ def find_executable_path(command: str):
     return False
 
 
+BUILTINS: dict[str, Response] = {"exit": exit, "echo": echo, "type": type, "pwd": pwd}
+
+
 def main():
-    sys.stdout.write("$ ")
-
     # Read
+    sys.stdout.write("$ ")
     raw_input = input()
-
-    # Evaluate input
-    response = None
 
     args = raw_input.split()
     first_command = args[0]
 
-    full_command_path = find_executable_path(first_command)
+    # Evaluate input
+    response = None
+    if first_command in BUILTINS:
+        response = BUILTINS[first_command](*args)
 
-    match full_command_path:
-        case False:
-            response = f"{first_command}: command not found"
-        case "exit":
-            # Check for early exit
-            return False
-        case "echo":
-            # Echo all other args
-            response = " ".join(args[1:])
-        case "type":
-            second_command = args[1]
-            executable_found = find_executable_path(second_command)
-            if second_command == executable_found:
-                response = f"{second_command} is a shell builtin"
-            elif executable_found:
-                response = f"{second_command} is {executable_found}"
-            else:
-                response = f"{second_command}: not found"
-        case "pwd":
-            response = os.getcwd()
-        case _:
-            # If valid command and not a builtin
-            # run and pass all args along
-            subprocess.run(args=args, executable=full_command_path)
+    elif find_executable_path(first_command):
+        # If valid command and not a builtin
+        # run and pass all args along
+        full_command_path = find_executable_path(first_command)
+        subprocess.run(args=args, executable=full_command_path)
 
-    # Print response
-    if response is not None:
+    else:
+        response = f"{first_command}: command not found"
+
+    # Print
+    if response:
         print(response)
+
+    # Loop if response is not False
+    if response is False:
+        return False
 
     return True
 
 
 if __name__ == "__main__":
-
     continue_running = True
     while continue_running:
         continue_running = main()
